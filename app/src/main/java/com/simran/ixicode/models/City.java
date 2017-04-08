e0f56588ad7fdd22a1dc9b0c1ef9530387103388
Simranjit Kour <simranjitk@intelligrape.com>
2017-04-08 17:19:18 +0530

    Modificatio in city class

--- app/src/main/java/com/simran/ixicode/models/City.java
+++ app/src/main/java/com/simran/ixicode/models/City.java
@@ -13,4 +13,62 @@ public class City {
     private String state;
     private String country;
 
+    public City() {
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getCt() {
+        return ct;
+    }
+
+    public void setCt(String ct) {
+        this.ct = ct;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
 }
